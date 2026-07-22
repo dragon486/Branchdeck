1155f73c96ef06d31cdea6ada7ae4ebedba3331f
dragon486 <adelmuhammed786@gmail.com>
2026-07-22 09:59:24 +0530

    Install and configure Vercel Analytics

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const outfit = Outfit({
@@ -67,6 +68,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
